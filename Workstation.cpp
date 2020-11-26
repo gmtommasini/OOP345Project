e17291d1b49d04a78c74b75bdf23a2f091e41fa7
gmtommasini <gmtommasini@gmail.com>
2020-11-26 10:10:01 -0500

    Final Project version MS3.0.1 Minor adjusts on the nextStation pointer

--- Workstation.cpp
+++ Workstation.cpp
@@ -1,7 +1,7 @@
 // Name: Guilherme Matsumoto Tommasini
 // Seneca Student ID: 167561182 
 // Seneca email: gmatsumoto-tommasini@myseneca.ca
-// Date of completion: 2020/11/24
+// Date of completion: 2020/11/26
 //
 // I confirm that I am the only author of this file
 //   and the content was created entirely by me.
@@ -10,7 +10,9 @@
 using namespace std;
 
 Workstation::~Workstation() {}
-Workstation::Workstation() {}
+Workstation::Workstation() {
+	m_pNextStation = nullptr;
+}
 Workstation::Workstation(const string& input) : Station(input), m_pNextStation{ nullptr } {}
 
 void Workstation::runProcess(std::ostream& os) {
@@ -24,7 +26,8 @@ bool Workstation::moveOrder() {
 	if (!m_orders.empty()) {
 		if (m_pNextStation != nullptr &&
 			m_orders.front().isItemFilled(this->getItemName())) {
-			*(Workstation*)m_pNextStation += std::move(m_orders.front());
+			//*(Workstation*)m_pNextStation += std::move(m_orders.front());
+			*static_cast<Workstation*>(m_pNextStation) += std::move(m_orders.front());
 			m_orders.pop_front();
 			moved = true;
 		}
